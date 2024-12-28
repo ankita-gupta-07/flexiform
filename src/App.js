b09d8c5ec7ef0c4dc250ccffd3f197471794d62a
Ankita Gupta <ankitag079@gmail.com>
2024-12-27 22:00:48 -0800

    first push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+      Click Me
+    </button>
+  );
+}
+
+export default App;
