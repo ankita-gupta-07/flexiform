a0e404358f83b86aaebe583d8d065a1c9835e1eb
Ankita Gupta <ankitag079@gmail.com>
2025-01-04 17:21:18 -0800

    change

--- src/components/DragDrop/Field.tsx
+++ src/components/DragDrop/Field.tsx
@@ -17,6 +17,8 @@ export const Field: React.FC<FieldProps> = ({ type }) => {
           <option value="">Select an option</option>
           <option value="1">Option 1</option>
           <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+          <option value="3">Option 4</option>
         </select>
       )}
       {type === "Checkbox" && (
